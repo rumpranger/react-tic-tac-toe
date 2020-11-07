8529dac7ec00203d430f593121826f79df2b6997
Charles Weiss <charlesw@ex-situ.com>
2020-11-07 11:03:09 -0500

    MOD updated next player notification

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
