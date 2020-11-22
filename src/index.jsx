3fa92bced820710d4f839999f713046e0cc6733f
Charles Weiss <charlesw@ex-situ.com>
2020-11-22 10:51:13 -0500

    MOD: Renamed js to more precise react jsx extension

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { unstable_concurrentAct } from 'react-dom/test-utils';
+import Game from './Components/Game/game'
+import './index.css';
+
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
