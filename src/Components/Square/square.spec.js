a9108951a784b39d3379eb9518310c8d0d10a5e6
Charles Weiss <charlesw@ex-situ.com>
2020-11-22 12:30:13 -0500

    MOD: testing webpack install

--- src/Components/Square/square.spec.js
+++ src/Components/Square/square.spec.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { mount } from 'cypress-react-unit-test'
 import { Square } from './square'
+import '../../index.css'
 
 describe('Square', () => {
-    it('renders value', () => {
-      mount(<Square value="X" />)
-      cy.contains('.square', 'X')
-    })
+  it('renders value', () => {
+    mount(<Square value="X" onClick={cy.stub().as('click')} />)
+    cy.contains('.square', 'X').click()
+    cy.get('@click').should('be.called')
   })
+})
