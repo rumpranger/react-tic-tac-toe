114d81cd4ff6c5baa35778c4847876eab542d5f5
Charles Weiss <charlesw@ex-situ.com>
2020-11-22 11:24:13 -0500

    MOD: Added test for reset button

--- cypress/integration/init.spec.js
+++ cypress/integration/init.spec.js
@@ -4,6 +4,6 @@ describe('Cypress', () => {
       expect(true).to.equal(true)
     })
     it('visits the app', () => {
-        cy.visit('http://localhost:3000')
+        cy.visit('/')
       })
   })
