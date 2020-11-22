f04d3f940cd36d805dd9645103754af85af7dd21
Charles Weiss <charlesw@ex-situ.com>
2020-11-22 11:11:56 -0500

    MOD: Added cypress test to verify cypress is initialized and app loads

---
+++ cypress/integration/init.spec.js
@@ -0,0 +1,9 @@
+/* Verify that Cypress actually works */
+describe('Cypress', () => {
+    it('is working', () => {
+      expect(true).to.equal(true)
+    })
+    it('visits the app', () => {
+        cy.visit('http://localhost:3000')
+      })
+  })
